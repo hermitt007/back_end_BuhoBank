daa07007332e8b596c686684b26251b464af2642
hermitt007 <145285118+hermitt007@users.noreply.github.com>
2024-07-09 20:06:25 -0500

    Update crud.py
    Aqui le hice los cambios para que no se repita el ci, el usuario y el email.

--- app/crud.py
+++ app/crud.py
@@ -1,10 +1,28 @@
-from database import customer_collection
-from models import CustomerModel
+from .database import customer_collection
+from .models import CustomerModel
+from .models import LogInModel
 from bson import ObjectId
+import bcrypt
 
 async def add_customer(customer_data: CustomerModel) -> dict:
+    # Verificar si el email, CI o usuario ya existe
+    existing_customer = await customer_collection.find_one({
+        "$or": [
+            {"email": customer_data.email},
+            {"ci": customer_data.ci},
+            {"user": customer_data.user}
+        ]
+    })
+    
+    if existing_customer:
+        raise ValueError("El CI, usuario o email ya existe")
+
+    # Hashear la contraseña antes de almacenarla
+    hashed_password = bcrypt.hashpw(customer_data.password.encode('utf-8'), bcrypt.gensalt())
+    
     # Convertir el modelo a un diccionario serializable
     customer_dict = customer_data.dict(by_alias=True)
+    customer_dict['password'] = hashed_password.decode('utf-8')  # Almacenar la contraseña hasheada
 
     # Insertar el cliente en la base de datos y obtener el ID insertado
     result = await customer_collection.insert_one(customer_dict)
@@ -18,3 +36,11 @@ async def add_customer(customer_data: CustomerModel) -> dict:
         new_customer['_id'] = str(new_customer['_id'])
 
     return new_customer
+
+async def checkData(customer_data: CustomerModel) -> bool:
+    query = {
+        "email": customer_data.email,
+        "password": customer_data.password
+    }
+    exists = await customer_collection.find_one(query)
+    return exists is not None

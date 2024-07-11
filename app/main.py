55c42557f57dbff6cb78bca239d43aa45d442c2e
hermitt007 <145285118+hermitt007@users.noreply.github.com>
2024-07-10 21:58:30 -0500

    Verificacion de usuarios, cedula y correo ya creados.

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from .models import CustomerModel, LogInModel
 from .crud import add_customer
 from .crud import checkData
 from fastapi.encoders import jsonable_encoder
-from .verifyData import verifyDataCI
+from .verifyData import verifyDataCI, verifyDataEmail,verifyDataUser
 
 
 app = FastAPI()
@@ -24,6 +24,12 @@ async def create_customer(customer: CustomerModel):
     ci=await verifyDataCI(customer)
     if ci:
         response=jsonable_encoder({"code":"CI_REPEAT"})
+        return JSONResponse(status_code=201, content=response)
+    elif await verifyDataUser(customer):
+        response = jsonable_encoder({"code": "USER_REPEAT"})
+        return JSONResponse(status_code=201, content=response)
+    elif await verifyDataEmail(customer):
+        response = jsonable_encoder({"code": "EMAIL_REPEAT"})
         return JSONResponse(status_code=201, content=response)
     else:
         new_customer = await add_customer(customer)

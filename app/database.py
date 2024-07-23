3fd6274bd96fab6b658a2775f9b921b9a3ca2824
hermitt007 <145285118+hermitt007@users.noreply.github.com>
2024-07-22 21:02:42 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.errors import ConnectionFailure
 
-MONGO_DETAILS = "mongodb://localhost:27017"
+MONGO_DETAILS = "mongodb+srv://buhobanco:cB5W7tVdZxuUQYWN@buhobanco.tpw58ga.mongodb.net/?retryWrites=true&w=majority&appName=BuhoBanco"
 
 client = AsyncIOMotorClient(MONGO_DETAILS)
 
